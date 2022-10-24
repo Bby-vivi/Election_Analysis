f54e41d4fa2baff89d5456293b9370b7ccd100ed
Bby-vivi <114452770+Bby-vivi@users.noreply.github.com>
2022-10-24 14:24:32 -0400

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,50 @@
+#The data we need to retrieve.
+#1. The total number of votes cast
+#2. A complete list of candidates who received votes
+#3. The percentage of votes each candidate won
+#4. The total number of votes each candidate won
+#5. The winner of the election based on popular vote
+
+# Add our dependencies
+
+import csv
+import os
+
+# Assign a variable for the file to load and the path
+file_to_load = os.path.join("Resources","election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+
+with open(file_to_load) as election_data:
+
+# Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+# Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+# Print each row in the csv file.
+    for row in file_reader:
+        print(row)
+
+# Close the file.
+    election_data.close()
+
+# Create a filename variable to a direct or indirect path to the file.
+    file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Use the open statement to open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+  # Write three counties to the file.
+    txt_file.write("Arapahoe\nDenver\nJefferson")
+
+   
+# Close the file
+txt_file.close()
+
+
